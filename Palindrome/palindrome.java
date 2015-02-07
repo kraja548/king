cfccc7372d85aa3774e3bab1fa61d801c77d5f26
raja <kraja548@gmail.com>
2015-02-07 15:04:56 -0800

    palindrome over]

---
+++ Palindrome/palindrome.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author admn
+ */
+public class palindrome 
+{
+    public static void main(String args[]) throws IOException
+    {
+        String original, reverse = "";
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);        
+        original = reader.readLine();
+        original=original.toLowerCase();
+        original=original.replaceAll("[^a-zA-Z]+","");
+        int length = original.length();
+        for ( int i = length - 1; i >= 0; i-- )
+        reverse = reverse + original.charAt(i);
+        if (original.equals(reverse))
+            System.out.println("YES");
+        else
+         System.out.println("NO");
+    }
+            
+            
+}
