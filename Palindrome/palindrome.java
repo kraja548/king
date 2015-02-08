f85d62984ea626e35b4c4481d92a3c6328446071
raja <kraja548@gmail.com>
2015-02-07 16:23:54 -0800

    palindrome over

--- Palindrome/palindrome.java
+++ Palindrome/palindrome.java
@@ -27,9 +27,9 @@ public class palindrome
         for ( int i = length - 1; i >= 0; i-- )
         reverse = reverse + original.charAt(i);
         if (original.equals(reverse))
-            System.out.println("Yes");
+            System.out.print("Yes");
         else
-         System.out.println("No");
+         System.out.print("No");
     }
             
             
